e22f7215985b14320efd6db974a5010bdde56908
Ann Bellinger <ann.t.bellinger@gmail.com>
2013-04-30 16:32:17 -0300

    Update game.js

--- frogger/game.js
+++ frogger/game.js
@@ -513,7 +513,7 @@ function time_set()
 
 function sendscores()
 {
-	var data = {'username':'Ann', 'game_title':'Frogger','score':'80','created_at':'sometime'};
+	var data = {'username':alert("YOU FAIL"), 'game_title':'Frogger','score':'80','created_at':'sometime'};
 //	var content = JSON.stringify(data);
 	$.post('http://warm-depths-5834.herokuapp.com/submit.json', data, function(data){
 		console.log(data)
